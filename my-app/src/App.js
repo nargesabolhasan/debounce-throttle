98c7bd3734f89c5ae5b52e955316d56aa70e0e70
narges Hajabolhasan <narges.abolhasan@gmail.com>
2022-04-28 09:35:01 +0430

    eact added

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Debounce from './component/Debounce';
+
+function App() {
+  return (
+    <div className="App">
+     <Debounce/>
+    </div>
+  );
+}
+
+export default App;
